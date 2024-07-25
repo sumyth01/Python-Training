e319bd931428cd610762ba99b8d99452bd5cbd34
sumyth <sumyth01@gmail.com>
2024-07-25 09:01:07 +0545

    Control flow

---
+++ Chapter 2 Control Flow/If_statement.py
@@ -0,0 +1,22 @@
+
+
+
+
+# if condition:
+#     code to execute if the condition is true
+# elif another_condition:
+#     code to execute if the another_ condition is True
+# else:
+#     code to execute if all condition are False
+
+
+
+
+age = 18
+
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
