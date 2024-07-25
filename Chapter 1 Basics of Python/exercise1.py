6735d4be838d9dabdcae2c095c2be82e6f3281d0
sumyth <sumyth01@gmail.com>
2024-07-25 08:45:23 +0545

    Basic exercise

---
+++ Chapter 1 Basics of Python/exercise1.py
@@ -0,0 +1,22 @@
+
+
+
+
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+
+print("sum:", sum_result)
+print("Difference:", difference)
+print("product:",product)
+print("Quotient:", quotient)
