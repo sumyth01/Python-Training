389bbcb26247312acbdefc7e72a6b31a931e4ab5
sumyth <sumyth01@gmail.com>
2024-08-01 11:13:22 +0545

    8/1/2024

---
+++ Chapter 2 Control Flow/Break_and_Continue_Statements.py
@@ -0,0 +1,11 @@
+
+
+
+
+# Break and continue example
+for i in range(10):
+    if i == 5:
+        break
+    if i % 2 == 0:
+        continue
+    print(i)
---
+++ Chapter 2 Control Flow/Pass_Statement.py
@@ -0,0 +1,8 @@
+
+
+
+# Pass statement example
+for i in range(5):
+    if i == 3:
+        pass  # Placeholder for future code
+    print(i)
